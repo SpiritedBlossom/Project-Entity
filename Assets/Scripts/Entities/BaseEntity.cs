03d867e45470f5dee727485109dbfd74191a406a
sujansooriyakumar <43353100+sujansooriyakumar@users.noreply.github.com>
2025-10-27 11:56:39 -0600

    base entity fixes

--- Assets/Scripts/Entities/BaseEntity.cs
+++ Assets/Scripts/Entities/BaseEntity.cs
@@ -13,19 +13,14 @@ public abstract class BaseEntity : MonoBehaviour {
     List<EntityAttack> entityAttacks;
     List<EntityAttackDistribution> attackDistribution;
 
-    // Deliberately kept empty at the moment per Sujan - (10/21/25 | Snype)
-    void Patrol() {
-    }
+    public abstract void Patrol();
 
     void AddDistribution(EntityAttack attack_, int weight_) {
-        /* Checking to see if an attack already exists in the moveset (?) and adding the entry's
-             * weight_ to the existing weight, to indicate that the entity should have a
-             * higher percentage of doing a certain attack (?) - (10/21/25 | Snype)
-             */
+       
 
         for (int i = 0; i < attackDistribution.Count; i++) {
             if (attack_ == attackDistribution[i].attack) {
-                attackDistribution[i].weight = weight_;
+                attackDistribution[i].weight += weight_;
                 return;
             }
         }
