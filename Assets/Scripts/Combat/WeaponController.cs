1e417c12ea967faea559ac91cc2a18e1e63bccc5
VvnZheng <vvnezheng@gmail.com>
2025-11-13 22:32:13 -0800

    Finished testing files and functions
    Fixed index ranges and added debug logs. Fixed raycast direction.

--- Assets/Scripts/Combat/WeaponController.cs
+++ Assets/Scripts/Combat/WeaponController.cs
@@ -2,15 +2,44 @@ using JetBrains.Annotations;
 using NUnit.Framework;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using System.Collections.Generic;
+using System;
+using static UnityEngine.UI.Image;
 
 
 public class WeaponController : MonoBehaviour
 {
+    [SerializeField]
     public List<WeaponSO> weaponSO;
-    private List<WeaponData> weaponData;
-    private WeaponData activeWeapon;
-    private int weaponIndex;
+    public List<WeaponData> weaponData = new List<WeaponData>();
+    public WeaponData activeWeapon;
+    public int weaponIndex;
+
+    public void Start()
+    {
+        Debug.Log("WeaponSO List: " + weaponSO);
+        InitializeWeapons();
+        Debug.Log("WeaponData List: " + weaponData);
+        for(int i = -1; i <= weaponSO.Count; i++)
+        {
+            SwitchWeapon(i);
+        }
+        ShootWeapon();
+    }
+    public void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Debug.Log("previous weapon!");
+            PreviousWeapon();
+        }
+        if (Mouse.current.rightButton.wasPressedThisFrame)
+        {
+            Debug.Log("next weapon!");
+            NextWeapon();
+        }
+    }
 
     public void InitializeWeapons()
     {
@@ -18,36 +47,42 @@ public class WeaponController : MonoBehaviour
         {
             WeaponData newWeaponData = new WeaponData();
             newWeaponData.InitializeWeaponData(weapon);
+            Debug.Log(weapon.weaponName);
+            Debug.Log(newWeaponData.weaponName);
             weaponData.Add(newWeaponData);
         }
     }
 
     public void SwitchWeapon(int index)
     {
-        if(index <= weaponData.Count)
+        if (index < weaponData.Count && index >= 0)
         {
             weaponIndex = index;
-            activeWeapon = weaponData[index];
+            activeWeapon = weaponData[index]; 
+            Debug.Log("weapon switched: " + weaponIndex + " " + activeWeapon.weaponName);
         }
+        else Debug.Log("out of range, invalid");
     }
 
     public void NextWeapon()
     {
-        if (weaponIndex == weaponSO.Count)
+        if (weaponIndex == weaponSO.Count-1)
         {
             weaponIndex = 0;
         } else weaponIndex++;
         activeWeapon = weaponData[weaponIndex];
+        Debug.Log("Now: " + weaponIndex + " " + activeWeapon.weaponName);
     }
 
     public void PreviousWeapon()
     {
         if (weaponIndex == 0)
         {
-            weaponIndex = weaponSO.Count;
+            weaponIndex = weaponSO.Count-1;
         }
         else weaponIndex--;
         activeWeapon = weaponData[weaponIndex];
+        Debug.Log("Now: " + weaponIndex + " " + activeWeapon.weaponName);
     }
 
     private void InitializeWeapon(WeaponSO weapon)
@@ -57,18 +92,21 @@ public class WeaponController : MonoBehaviour
         //add it to weaponData
         WeaponData newWeaponData = new WeaponData();
         newWeaponData.InitializeWeaponData(weapon);
-        activeWeapon = newWeaponData; 
+        activeWeapon = newWeaponData;
+        Debug.Log(activeWeapon.weaponName);
         weaponData.Add(newWeaponData);
     }
 
     public void ShootWeapon()
     {
+        Debug.Log("Shooting");
         //raycast
-        Ray ray = new Ray(transform.position, (Camera.main.transform.position));
-        RaycastHit hitData; 
+        Ray ray = new Ray(Camera.main.transform.position, (Camera.main.transform.forward));
+        RaycastHit hitData;
+        Debug.DrawRay(ray.origin, ray.direction * 10);
         if (Physics.Raycast(ray, out hitData, activeWeapon.range))
         {
-            print("hit");
+            print("the ray hit something!");
         }
     }
 
