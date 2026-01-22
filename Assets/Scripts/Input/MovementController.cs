e607c7dcbc831075811ffa1273548c1016e09d48
sujansooriyakumar <43353100+sujansooriyakumar@users.noreply.github.com>
2026-01-22 14:38:14 -0700

    change movement to physics

--- Assets/Scripts/Input/MovementController.cs
+++ Assets/Scripts/Input/MovementController.cs
@@ -5,26 +5,32 @@ public class MovementController : MonoBehaviour
 {
     private Vector2 moveVector;
     private Coroutine movementCoroutine;
+    private Rigidbody rb;
     [SerializeField] private float moveSpeed;
     [SerializeField] private PlayerInputController controller;
 
     private void Start()
     {
         controller.onMoveInputReceived += BeginMovement;
+
+        rb = GetComponent<Rigidbody>();
     }
     public void BeginMovement(Vector2 moveVector_)
     {
         moveVector = moveVector_;
-        StartCoroutine(MovementCoroutine());
      
     }
 
-    private IEnumerator MovementCoroutine()
+    private void FixedUpdate()
     {
-        while (true)
-        {
-            transform.position += new Vector3(moveVector.x, 0, moveVector.y) * Time.deltaTime * moveSpeed;
-            yield return new WaitForEndOfFrame();
-        }
+        Vector3 input = new Vector3(moveVector.x, 0f, moveVector.y);
+
+        Vector3 moveDirection = transform.TransformDirection(input).normalized;
+
+        Vector3 targetVelocity = moveDirection * moveSpeed;
+        targetVelocity.y = rb.linearVelocity.y;
+
+        rb.linearVelocity = targetVelocity;
     }
+
 }
